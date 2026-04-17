66e83d1c65d9042fccf2eb1785167da30605aec2
Shaigro <robin.boucher@epita.fr>
2026-04-17 02:36:37 +0200

    remove dead code from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,84 +2,14 @@ import {
   AppShell,
   Avatar,
   Box,
-  Center,
-  Container,
-  Divider,
   Group,
-  Image,
-  MultiSelect,
   Stack,
   Text,
   Title,
-  type MultiSelectProps,
 } from "@mantine/core";
-import { ItemNeighbors } from "./ItemNeighbors";
-import items from "./assets/items.json";
-import { useMemo, useState } from "react";
-import { Item } from "./Item";
+import { useState } from "react";
 import { WantedPanel } from "./WantedPanel";
 import { FloorPanel } from "./FloorPanel";
-
-//   return (
-//     <Container fluid style={{ textAlign: "center" }}>
-//       <h1>TBOI spindown helper</h1>
-//       <Group mx="xl" justify="space-between" gap="sm">
-//         <section>
-//           <h2>Items to spindown into</h2>
-//           <Center>
-//             <Stack>
-//               <MultiSelect
-//                 renderOption={renderMultiSelectOption}
-//                 label="Items wanted"
-//                 placeholder="Pick items"
-//                 data={availableNames}
-//                 searchable
-//                 value={namesSpindown}
-//                 onChange={setNamesSpindown}
-//                 limit={50}
-//               />
-//               {namesSpindown.length === 0 && <ItemNeighbors index={-1} />}
-//               {namesSpindown.map((name) => (
-//                 <ItemNeighbors key={name} index={indicesByNames[name]} />
-//               ))}
-//             </Stack>
-//           </Center>
-//         </section>
-//         <Divider orientation="vertical" size="xl" />
-//         <section>
-//           <h2>Available items on the floor</h2>
-//           <Center>
-//             <Stack>
-//               {namesFloor.map((name) => {
-//                 const floorIndex = indicesByNames[name];
-//                 return (
-//                   <Group key={floorIndex}>
-//                     <Item index={floorIndex} />{" "}
-//                     <Divider orientation="vertical" />{" "}
-//                     {namesSpindown.length === 0 && "Pick 1 item wanted"}
-//                     <Stack gap="0px">
-//                       {namesSpindown.map((spindown) => {
-//                         const spindownIndex = indicesByNames[spindown];
-//                         const diff = floorIndex - spindownIndex;
-
-//                         return diff < 0 ? null : (
-//                           <Group>
-//                             <Item index={spindownIndex} inline />
-//                             <span>: {diff} spindown</span>
-//                           </Group>
-//                         );
-//                       })}
-//                     </Stack>
-//                   </Group>
-//                 );
-//               })}
-//             </Stack>
-//           </Center>
-//         </section>
-//       </Group>
-//     </Container>
-//   );
-// }
 
 function App() {
   const [lookingForItems, setLookingForItems] = useState<string[]>([]);
